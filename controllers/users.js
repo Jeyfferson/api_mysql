58c630ec6cec761121f8f491777fda641e30961a
Jeyfferson <jeyffesilv@gmail.com>
2023-07-08 14:02:47 -0300

    Adicionando uma router para listar dados, retornando em formado objeto json

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,32 @@ const router = express.Router()
 //Incluir o arquivo que possui a conexão com o banco de dados
 const db = require('./../db/models')
 
+//Criar rota de listar
+router.get("/users", async (req, res) => {
+   //Recuperar todos os usuarios do banco
+   const users = await db.Users.findAll({
+      //Iniciar quais colunas recuperar
+      attributes: ['id','name','email'],
+
+      //Ordenar os registros pela coluna id na forma descrescente
+      order:[['id', 'DESC']]   
+   })
+
+   if(users){
+      //Pausar o processamento e retornar os dados em formato de objeto
+      return res.json({
+         users: users
+      })
+   }else{
+      //Pausar o processamento e retornar a mensagem de erro!
+      return res.status(400).json({
+         message: "Nenhum Usuário encontrado!"
+      })
+   }
+})
+
+
+//Rota de cadastro
 router.post(`/users`, async (req, res) => {
 
    //Receber os dados enviados no body request
@@ -20,7 +46,9 @@ router.post(`/users`, async (req, res) => {
       });
    }).catch((erro) =>{
       //Pausar o processamento e retornar a mensagem de erro!
-      console.log('Usuário não cadastrado com sucesso!');
+      return res.json({
+         message: "Usuário não cadastrado com sucesso!"
+      })
    })
 })
 
